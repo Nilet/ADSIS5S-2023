afd226754098a3261bd5368af1042b3c431a970d
Nilet <nilet@tutanota.com>
2023-03-15 18:34:21 -0300

    saving on desktop to finish at college

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ class App {
 
         this.middleware()
         this.routes()
-        this.database()
+        // this.database()
     }
 
     public middleware(): void {
@@ -25,18 +25,18 @@ class App {
     public routes(): void {
         this.express.use(routes)
     }
-    private async database(){
-    const data = fs.readFileSync('db.json');
-    const jsonData = JSON.parse(data.toString());
-    const authKey = jsonData.auth;
-        try{
-            mongoose.set("strictQuery", true)
-            await mongoose.connect(authKey)
-            console.log("Success connecting database")
-        }catch(e){
-            console.log(e)
-        }
-    }
+    // private async database(){
+    // const data = fs.readFileSync('db.json');
+    // const jsonData = JSON.parse(data.toString());
+    // const authKey = jsonData.auth;
+    //     try{
+    //         mongoose.set("strictQuery", true)
+    //         await mongoose.connect(authKey)
+    //         console.log("Success connecting database")
+    //     }catch(e){
+    //         console.log(e)
+    //     }
+    // }
 }
 
 export default new App().express
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express'
 import healthCheckController from './controller/healthCheckController'
 import userController from './controller/userController'
+import productController from './controller/productController'
+
 
 
 const routes = Router()
@@ -8,5 +10,6 @@ const routes = Router()
 routes.get('/health-check', healthCheckController.check)
 routes.get('/users', userController.getNome)
 routes.get('/users/:nome', userController.getUserId)
+routes.post('/products', productController.postProduct)
 
 export default routes
